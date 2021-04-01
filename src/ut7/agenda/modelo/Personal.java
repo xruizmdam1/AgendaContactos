fe985c7a5b785ce21c887351563c5c5afe13cf79
cgoncalvesadam1 <cgoncalvesadam1@gmail.com>
2021-04-01 19:16:34 +0200

    Actualizada Clase Personal

--- src/ut7/agenda/modelo/Personal.java
+++ src/ut7/agenda/modelo/Personal.java
@@ -1,9 +1,26 @@
 package ut7.agenda.modelo;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Personal extends Contacto {
 
-	public Personal() {
+	private LocalDate fechaNac;
+	private String fechaCorrecta;
+	private Relacion relacion;
 
+	public Personal(String nombre, String apellidos, String telefono, String email, String fecha, Relacion relacion) {
+		super(nombre, apellidos, telefono, email);
+		this.fechaNac = fechaNac;
+		this.relacion = relacion;
+
+		fechaNac = LocalDate.parse(fecha);
+		DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd MMM yyyy");
+		fechaCorrecta = fechaNac.format(formateador);
 	}
 
+	public String toString() {
+		super.toString();
+		return ("Fecha Nacimiento: " + fechaCorrecta + "\n" + "Relación: " + relacion);
+	}
 }
