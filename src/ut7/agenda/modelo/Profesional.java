929200016972b6d7f64ef626cfd09fe19757f484
atroyaarev <atroyaarev@gmail.com>
2021-04-09 00:22:08 +0200

    corregir metodo felicitar

--- src/ut7/agenda/modelo/Profesional.java
+++ src/ut7/agenda/modelo/Profesional.java
@@ -4,9 +4,7 @@ import java.util.Random;
 
 public class Profesional extends Contacto {
 	private String nombreGuardado ;
-    public enum firma {
-        Atentamente, Saludos, Saludos_Cordiales, Mis_Mejores_Deseos
-    };
+    
 		public Profesional(String nombre, String apellidos, String telefono,
 				String email) {
 		
@@ -18,12 +16,23 @@ public class Profesional extends Contacto {
 					    if (caracteres[i] == ' ' || caracteres[i] == '.' || caracteres[i] == ',')
 					      caracteres[i + 1] = Character.toUpperCase(caracteres[i + 1]);
 				 nombreGuardado = new String(caracteres);
-				int emails = new Random().nextInt(firma.values().length);
-				email = String.valueOf(emails);
-
+				
 				
 				 nombre = nombreGuardado;
 		}
+				public String getNombreGuardado() {
+			return nombreGuardado;
+		}
+		public void setNombreGuardado(String nombreGuardado) {
+			this.nombreGuardado = nombreGuardado;
+		}
+		@Override
+		public String getFirmaEmail() {
+		    int firma = new Random().nextInt(Firma.values().length);
+		   String email = String.valueOf(firma);
+		   return email;
+		}
+	
 		@Override
 		public String toString() {
 			return "Profesional [nombreGuardado=" + nombreGuardado + ", getNombreGuardado()=" + getNombreGuardado()
@@ -32,12 +41,5 @@ public class Profesional extends Contacto {
 					+ ", getPrimeraLetra()=" + getPrimeraLetra() + ", toString()=" + super.toString() + ", getClass()="
 					+ getClass() + "]";
 		}
-		public String getNombreGuardado() {
-			return nombreGuardado;
-		}
-		public void setNombreGuardado(String nombreGuardado) {
-			this.nombreGuardado = nombreGuardado;
-		}
-	
-		
+
 }
