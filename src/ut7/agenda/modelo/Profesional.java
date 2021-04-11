b0052911f62d52d4eb40f57370f18a05e17751bd
atroyaarev <atroyaarev@gmail.com>
2021-04-11 14:09:40 +0200

    Corrección del constructor,añadido metodo para capitalizar el nombre de la

--- src/ut7/agenda/modelo/Profesional.java
+++ src/ut7/agenda/modelo/Profesional.java
@@ -3,43 +3,37 @@ package ut7.agenda.modelo;
 import java.util.Random;
 
 public class Profesional extends Contacto {
-	private String nombreGuardado ;
+	private String nombreEmpresa ;
 	private enum firma {Atentamente, Saludos, Saludos_cordiales, Mis_mejores_deseos};
-		public Profesional(String nombre, String apellidos, String telefono,
-				String email) {
-		
-			 super(nombre, apellidos, telefono, email);
-				
-				char[] caracteres = nombre.toCharArray();
-				caracteres[0] = Character.toUpperCase(caracteres[0]);
-				 for (int i = 0; i < nombre.length()- 2; i++) 
-					    if (caracteres[i] == ' ' || caracteres[i] == '.' || caracteres[i] == ',')
-					      caracteres[i + 1] = Character.toUpperCase(caracteres[i + 1]);
-				 nombreGuardado = new String(caracteres);
-				
-				
-				 nombre = nombreGuardado;
-		}
-				public String getNombreGuardado() {
-			return nombreGuardado;
-		}
-		public void setNombreGuardado(String nombreGuardado) {
-			this.nombreGuardado = nombreGuardado;
-		}
+	
+	public Profesional(String nombre, String apellidos, String telefono, String email, String nombreEmpresa) {
+		super(nombre, apellidos, telefono, email);
+		this.setNombreEmpresa(letraMayuscula(nombreEmpresa));
+	
+	}
+
+	public String getNombreEmpresa() {
+		return nombreEmpresa;
+	}
+
+	public void setNombreEmpresa(String nombreEmpresa) {
+		this.nombreEmpresa = nombreEmpresa;
+	}
 		@Override
 		public String getFirmaEmail() {
 			int aux = new Random().nextInt(firma.values().length);
 			String email = String.valueOf(aux);
 			return email;
-
 		}
-		@Override
-		public String toString() {
-			return "Profesional [nombreGuardado=" + nombreGuardado + ", getNombreGuardado()=" + getNombreGuardado()
-					+ ", getNombre()=" + getNombre() + ", getApellidos()=" + getApellidos() + ", getTelefono()="
-					+ getTelefono() + ", getEmail()=" + getEmail() + ", hashCode()=" + hashCode()
-					+ ", getPrimeraLetra()=" + getPrimeraLetra() + ", toString()=" + super.toString() + ", getClass()="
-					+ getClass() + "]";
-		}
+		
+			private String letraMayuscula (String nombreEmpresa){
+				 
+				if (nombreEmpresa.length() == 0 ) {
+					return nombreEmpresa;
+				}
+				return nombreEmpresa.substring(0, 1).toUpperCase() +
+						nombreEmpresa.substring(1).toLowerCase();
+			 
+			}
 
 }
