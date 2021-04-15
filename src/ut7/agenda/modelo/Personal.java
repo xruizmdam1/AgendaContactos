6a67b6e84aa7a37ea4e44ce36ed1fab58af34d30
cgoncalvesadam1 <cgoncalvesadam1@gmail.com>
2021-04-15 20:07:46 +0200

    Corregida clase Personal.

--- src/ut7/agenda/modelo/Personal.java
+++ src/ut7/agenda/modelo/Personal.java
@@ -3,28 +3,26 @@ package ut7.agenda.modelo;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/* 
+ * @author CATARINA
+ */
+
 public class Personal extends Contacto {
 
 	private LocalDate fechaNac;
-	private String fechaCorrecta;
 	private Relacion relacion;
-	private String fecha;
 
-	public Personal(String nombre, String apellidos, String telefono, String email, String fecha, Relacion relacion) {
+	public Personal(String nombre, String apellidos, String telefono, String email, String fechaNac,
+			Relacion relacion) {
 		super(nombre, apellidos, telefono, email);
-		this.fecha = fecha;
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		this.fechaNac = LocalDate.parse(fechaNac, dtf);
 		this.relacion = relacion;
 
-		fechaNac = LocalDate.parse(fecha);
-		DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd MMM yyyy");
-		fechaCorrecta = fechaNac.format(formateador);
 	}
 
 	public boolean esCumpleaños(String fecha) {
-		LocalDate ld = LocalDate.parse(fecha);
-		DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd MMM yyyy");
-		String fechaCumple = ld.format(formateador);
-		return fechaCorrecta == fechaCumple;
+		return fechaNac.isEqual(LocalDate.now());
 	}
 
 	public String getFirmaEmail() {
@@ -33,6 +31,6 @@ public class Personal extends Contacto {
 
 	public String toString() {
 		super.toString();
-		return ("Fecha Nacimiento: " + fechaCorrecta + "\n" + "Relación: " + relacion);
+		return ("Fecha Nacimiento: " + fechaNac + "\n" + "Relación: " + relacion);
 	}
 }
