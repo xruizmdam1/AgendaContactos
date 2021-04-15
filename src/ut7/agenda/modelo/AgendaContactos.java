bed0153813d4e73743fcb88144d6607b26c6cefd
cgoncalvesadam1 <cgoncalvesadam1@gmail.com>
2021-04-15 20:51:19 +0200

    Actualizada agenda Contactos.

--- src/ut7/agenda/modelo/AgendaContactos.java
+++ src/ut7/agenda/modelo/AgendaContactos.java
@@ -1,10 +1,15 @@
 package ut7.agenda.modelo;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
+
+/* 
+ * @author XABIER, CATARINA, ANTHONNY
+ */
 
 public class AgendaContactos {
 	private Map<Character, Set<Contacto>> agenda;
@@ -29,29 +34,49 @@ public class AgendaContactos {
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append(false);
+
 		return sb.toString();
 	}
 
 	public List<Contacto> buscarContactos(String texto) {
+		ArrayList<Contacto> buscarContactos = new ArrayList<>();
 
-		return null;
-
+		Set<Map.Entry<Character, Set<Contacto>>> grupo = agenda.entrySet();
+		Iterator<Map.Entry<Character, Set<Contacto>>> it = grupo.iterator();
+		while (it.hasNext()) {
+			Map.Entry<Character, Set<Contacto>> map = it.next();
+			for (Contacto contacto : map.getValue()) {
+				if (contacto.getApellidos().equals(texto) || contacto.getNombre().equals(texto)) {
+					buscarContactos.add(contacto);
+				}
+			}
+		}
+		return buscarContactos;
 	}
 
 	public List<Personal> personalesEnLetra(char letra) {
+		ArrayList<Personal> personalesEnLetra = new ArrayList<>();
 
+		Set<Map.Entry<Character, Set<Contacto>>> grupito = agenda.entrySet();
+		Iterator<Map.Entry<Character, Set<Contacto>>> it = grupito.iterator();
+		while (it.hasNext()) {
+			Map.Entry<Character, Set<Contacto>> mapita = it.next();
+			for (Contacto contacto : mapita.getValue()) {
+
+			}
+		}
 		return null;
 	}
 
 	public List<Personal> felicitar() {
 		ArrayList<Personal> lista = new ArrayList<Personal>();
 		String fecha = "";
-		for(Personal persona : lista) {
-			if(persona.esCumpleaños( fecha)) {
+		for (Personal persona : lista) {
+			if (persona.esCumpleaños(fecha)) {
 				lista.add(persona);
 			}
 		}
-		
+
 		return lista;
 	}
 
--- src/ut7/agenda/test/TestAgenda.java
+++ src/ut7/agenda/test/TestAgenda.java
@@ -1,4 +1,5 @@
 package ut7.agenda.test;
+
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
@@ -9,7 +10,7 @@ import ut7.agenda.modelo.Contacto;
 import ut7.agenda.modelo.Personal;
 import ut7.agenda.modelo.Relacion;
 
-public class TestAgenda {
+class TestAgenda {
 
 	public static void main(String[] args) {
 		AgendaContactos agenda = new AgendaContactos();
@@ -61,16 +62,13 @@ public class TestAgenda {
 
 	}
 
-	private static void personalesOrdenadosPorFecha(AgendaContactos agenda,
-			char letra) {
-		System.out.println("Personales en letra " + letra
-				+ " ordenados de < a > fecha de nacimiento");
+	private static void personalesOrdenadosPorFecha(AgendaContactos agenda, char letra) {
+		System.out.println("Personales en letra " + letra + " ordenados de < a > fecha de nacimiento");
 		List<Personal> personales = agenda.personalesEnLetra(letra);
 		if (personales == null) {
 			System.out.println(letra + " no está en la agenda");
 		} else {
-			agenda.personalesOrdenadosPorFechaNacimiento(letra)
-					.forEach(contacto -> System.out.println(contacto));
+			agenda.personalesOrdenadosPorFechaNacimiento(letra).forEach(contacto -> System.out.println(contacto));
 		}
 
 	}
@@ -81,8 +79,7 @@ public class TestAgenda {
 	}
 
 	private static void separador() {
-		System.out.println(
-				"------------------------------------------------------------");
+		System.out.println("------------------------------------------------------------");
 
 	}
 
