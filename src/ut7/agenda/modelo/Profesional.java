8b1140bc1484d5d149851db8ac6e65b04630e464
cgoncalvesadam1 <cgoncalvesadam1@gmail.com>
2021-04-15 17:58:52 +0200

    Puesta etiqueta author

--- src/ut7/agenda/modelo/Profesional.java
+++ src/ut7/agenda/modelo/Profesional.java
@@ -1,15 +1,16 @@
 package ut7.agenda.modelo;
 
-import java.util.Random;
+/* 
+ * @author ANTHONNY
+ */
 
 public class Profesional extends Contacto {
-	private String nombreEmpresa ;
-	
-	
+	private String nombreEmpresa;
+
 	public Profesional(String nombre, String apellidos, String telefono, String email, String nombreEmpresa) {
 		super(nombre, apellidos, telefono, email);
 		this.setNombreEmpresa(letraMayuscula(nombreEmpresa));
-	
+
 	}
 
 	public String getNombreEmpresa() {
@@ -19,26 +20,27 @@ public class Profesional extends Contacto {
 	public void setNombreEmpresa(String nombreEmpresa) {
 		this.nombreEmpresa = nombreEmpresa;
 	}
-		@Override
-		public String getFirmaEmail() {
-			String[] firma = {"Atentamente", "Saludos", "Saludos cordiales", "Mis mejores deseos"}; //se crea un array para la firma 
-			return firma[(int) (Math.random() * 3)]; // se realiza un random
-		}
-		
-			private String letraMayuscula (String nombreEmpresa){
-				 
-				if (nombreEmpresa.length() == 0 ) {
-					return nombreEmpresa;
-				}
-				return nombreEmpresa.substring(0, 1).toUpperCase() +
-						nombreEmpresa.substring(1).toLowerCase();
-			 
-			}
 
-			@Override
-			public String toString() {
-				return "Profesional [nombreEmpresa=" + nombreEmpresa + ", getNombreEmpresa()=" + getNombreEmpresa()
-						+ ", getFirmaEmail()=" + getFirmaEmail() + "]";
-			}
+	@Override
+	public String getFirmaEmail() {
+		String[] firma = { "Atentamente", "Saludos", "Saludos cordiales", "Mis mejores deseos" }; // se crea un array
+																									// para la firma
+		return firma[(int) (Math.random() * 3)]; // se realiza un random
+	}
+
+	private String letraMayuscula(String nombreEmpresa) {
+
+		if (nombreEmpresa.length() == 0) {
+			return nombreEmpresa;
+		}
+		return nombreEmpresa.substring(0, 1).toUpperCase() + nombreEmpresa.substring(1).toLowerCase();
+
+	}
+
+	@Override
+	public String toString() {
+		return "Profesional [nombreEmpresa=" + nombreEmpresa + ", getNombreEmpresa()=" + getNombreEmpresa()
+				+ ", getFirmaEmail()=" + getFirmaEmail() + "]";
+	}
 
 }
