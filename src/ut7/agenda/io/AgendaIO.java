ae0110b7a37d5c1c72b6dd71f2c7cbb644e11e36
xruizmdam1 <xruizmdam1@gmail.com>
2021-05-17 18:09:30 +0200

    Arreglada clase AgendaIO (Problema File)

--- src/ut7/agenda/io/AgendaIO.java
+++ src/ut7/agenda/io/AgendaIO.java
@@ -6,6 +6,7 @@ package ut7.agenda.io;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
