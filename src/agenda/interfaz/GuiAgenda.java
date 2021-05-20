8f51b8c4fb6e4e4732157ef7796e4d7c865e56be
cgoncalvesadam1 <cgoncalvesadam1@gmail.com>
2021-05-20 17:16:51 +0200

    Añadido el for a crearPanelLetras

--- src/agenda/interfaz/GuiAgenda.java
+++ src/agenda/interfaz/GuiAgenda.java
@@ -92,47 +92,48 @@ public class GuiAgenda extends Application {
 	}
 
 	private VBox crearPanelBotones() {
-		
+
 		VBox panel = new VBox();
-			
-			txtBuscar = new TextField("Buscar");
-			txtBuscar.getStyleClass().add("text-field");
-			txtBuscar.setMinHeight(40);
-			VBox.setMargin(txtBuscar, new Insets(0,0,40,0));
-			
-			rbtListarTodo = new RadioButton("Listar toda la agenda");
-			rbtListarTodo.getStyleClass().add("radio-button");
-			
-			rbtListarSoloNumero = new RadioButton("Listar nº contactos");
-			rbtListarSoloNumero.getStyleClass().add("radio-button");
-			
-			btnListar = new Button("Listar");
-			btnListar.getStyleClass().add("botones");
-			btnListar.setPrefWidth(250);
-			VBox.setMargin(btnListar, new Insets(0, 0, 40, 0));
-			
-			btnPersonalesEnLetra = new Button("Contactos personales en letra");
-			btnPersonalesEnLetra.getStyleClass().add("botones");
-			btnPersonalesEnLetra.setPrefWidth(250);
-			
-			btnPersonalesOrdenadosPorFecha = new Button("Contactos personales\n ordenados por fecha");
-			btnPersonalesOrdenadosPorFecha.getStyleClass().add("botones");
-			btnPersonalesOrdenadosPorFecha.setPrefWidth(250);
-			
-			btnClear = new Button("Clear");
-			btnClear.getStyleClass().add("botones");
-			btnClear.setPrefWidth(250);
-			VBox.setMargin(btnClear, new Insets(40, 0, 0, 0));
-			
-			btnSalir = new Button("Salir");
-			btnSalir.getStyleClass().add("botones");
-			btnSalir.setPrefWidth(250);
-			
+
+		txtBuscar = new TextField("Buscar");
+		txtBuscar.getStyleClass().add("text-field");
+		txtBuscar.setMinHeight(40);
+		VBox.setMargin(txtBuscar, new Insets(0, 0, 40, 0));
+
+		rbtListarTodo = new RadioButton("Listar toda la agenda");
+		rbtListarTodo.getStyleClass().add("radio-button");
+
+		rbtListarSoloNumero = new RadioButton("Listar nº contactos");
+		rbtListarSoloNumero.getStyleClass().add("radio-button");
+
+		btnListar = new Button("Listar");
+		btnListar.getStyleClass().add("botones");
+		btnListar.setPrefWidth(250);
+		VBox.setMargin(btnListar, new Insets(0, 0, 40, 0));
+
+		btnPersonalesEnLetra = new Button("Contactos personales en letra");
+		btnPersonalesEnLetra.getStyleClass().add("botones");
+		btnPersonalesEnLetra.setPrefWidth(250);
+
+		btnPersonalesOrdenadosPorFecha = new Button("Contactos personales\n ordenados por fecha");
+		btnPersonalesOrdenadosPorFecha.getStyleClass().add("botones");
+		btnPersonalesOrdenadosPorFecha.setPrefWidth(250);
+
+		btnClear = new Button("Clear");
+		btnClear.getStyleClass().add("botones");
+		btnClear.setPrefWidth(250);
+		VBox.setMargin(btnClear, new Insets(40, 0, 0, 0));
+
+		btnSalir = new Button("Salir");
+		btnSalir.getStyleClass().add("botones");
+		btnSalir.setPrefWidth(250);
+
 		panel.setSpacing(10);
 		panel.setPadding(new Insets(10));
-		panel.getChildren().addAll(txtBuscar, rbtListarTodo, rbtListarSoloNumero, btnListar, btnPersonalesEnLetra, btnPersonalesOrdenadosPorFecha, btnClear, btnSalir);
+		panel.getChildren().addAll(txtBuscar, rbtListarTodo, rbtListarSoloNumero, btnListar, btnPersonalesEnLetra,
+				btnPersonalesOrdenadosPorFecha, btnClear, btnSalir);
 		return panel;
-		
+
 	}
 
 	private GridPane crearPanelLetras() {
@@ -180,6 +181,16 @@ public class GuiAgenda extends Application {
 		panel.getColumnConstraints().addAll(col1, col2, col3, col4, col5, col6, col7, col8);
 		panel.getRowConstraints().addAll(fila1, fila2);
 
+		String abecedario = "abcdefghijklmnñopqrstuvwxyz";
+		Button btnLetra;
+		int contadorCol = 0;
+		for (int i = 0; i < abecedario.length(); i++) {
+			btnLetra = new Button(String.valueOf(abecedario.charAt(i)));
+			panel.add(btnLetra, contadorCol, 0);
+
+			contadorCol++;
+
+		}
 		return panel;
 	}
 
